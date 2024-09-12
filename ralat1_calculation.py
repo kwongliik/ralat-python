ba276a21a7829860ab9190c30c177a1be6fcbaa1
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-09-12 13:46:25 +0800

    add files

---
+++ ralat1_calculation.py
@@ -0,0 +1,16 @@
+def calculation(a, b):    
+    division = a / b
+    return division
+
+def get_input():
+    x = int(input("Masukkan nombor integer pertama:"))
+    y = int(input("Masukkan nombor integer kedua:"))
+    return x, y
+
+def main():
+    (x, y) = get_input()
+    divide = calculation(x, y)    
+    print(f"Division = {divide}")
+
+if __name__ == "__main__":
+    main()
